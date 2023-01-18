0779370cc2aba778bb1f5aafe5c487cc9e046716
Oleg <olegashepelev@gmail.com>
2023-01-18 21:01:18 +0600

    Complete first task

---
+++ 02/Program.cs
@@ -0,0 +1,24 @@
+﻿int[] array = GetRandomArray(7, -10, 99);
+int sumOddPosition = FindSumOddPositions(array);
+
+Console.WriteLine($"[{String.Join(", ", array)}] -> {sumOddPosition}");
+
+int[] GetRandomArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+
+int FindSumOddPositions(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i += 2)
+    {
+        sum += array[i];
+    }
+    return sum;
+}
