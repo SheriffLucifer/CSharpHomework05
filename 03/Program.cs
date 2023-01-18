b0074c98e3a27bd4f88fc9c1ad0f3431812632fc
Oleg <olegashepelev@gmail.com>
2023-01-18 21:43:56 +0600

    Complete third task

---
+++ 03/Program.cs
@@ -0,0 +1,26 @@
+﻿double[] array = GetRandomArray(5, 0, 99);
+double differenceMaxMin = FindDifferenceMaxMin(array);
+
+Console.WriteLine($"[{String.Join(", ", array)}] -> {differenceMaxMin}");
+
+double[] GetRandomArray(int size, int minValue, int maxValue)
+{
+    double[] result = new double[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+
+double FindDifferenceMaxMin(double[] array)
+{
+    double minValue = array[0];
+    double maxValue = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] < minValue) minValue = array[i];
+        if (array[i] > maxValue) maxValue = array[i];
+    }
+    return maxValue - minValue;
+}
