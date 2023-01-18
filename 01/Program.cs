887998c49e9dd9c0c51eef73a639931ef0a338e5
Oleg <olegashepelev@gmail.com>
2023-01-18 18:36:29 +0600

    Complete first task

---
+++ 01/Program.cs
@@ -0,0 +1,24 @@
+﻿int[] array = GetRandomArray(50, 100, 999);
+int evenElements = FindEvenElements(array);
+
+Console.WriteLine($"[{String.Join(", ", array)}] -> {evenElements}");
+
+int[] GetRandomArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+
+int FindEvenElements(int[] array)
+{
+    int evenElement = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) evenElement++;
+    }
+    return evenElement;
+}
